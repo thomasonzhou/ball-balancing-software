56fa23930f6350a639d51270980968e4752ec466
Olivia Ye <oliviawxye@gmail.com>
2024-09-20 14:23:42 -0400

    more math

--- kinematics/plate_kinematics.py
+++ kinematics/plate_kinematics.py
@@ -6,11 +6,11 @@ from motor import Motor, MOTOR_LEG_LENGTH, PLATE_LEG_LENGTH
 
 ### Can be changed
 P_B_length = 5
-# See `plate_vectors.png`
+# See `motor_orientations.png`
 MOTOR_ORIENTATIONS = [
-    np.pi/2, # along the y-axis
-    7*np.pi/6, # 120 deg, CCW from A
-    11*np.pi/6, # 120, CW from A
+    np.pi/2, # Motor A: along the y-axis
+    7*np.pi/6, # Motor B: 120 deg, CCW from A
+    11*np.pi/6, # Motor C: 120, CW from A
 ]
 
 # HELPER CONSTANTS
@@ -28,29 +28,32 @@ def calculate_angle_from_cosine(a: float, b: float, c: float) -> float:
     return np.arccos((np.square(b) + np.square(c) - np.square(a))/(2*b*c))
 
 def calculate_theta_phi_from_N(N: npt.NDArray) -> tuple[float, float]:
-    """Given the desired height, find the tilt about the x and y axis. See `rotation_math.png` for more details.
+    """Given the normal vector of the plate, find the tilt about the x and y axis.
+    See `rotation_math.png` for more details.
     
-    Args: 
-        T (3 float vector): The vector with the desired plane height and direction
+    Args:
+        N (3 float vector): The normal vector with the desired plate tilt
     
     Returns:
         float: phi_x -> tilk about the x axis
-        float: theta_y -> tilt about the y axis"""
+        float: theta_y -> tilt about the y axis
+    """
     N_norm = N/np.linalg.norm(N)
     phi_x = np.arcsin(N_norm[1])
     theta_y = np.arcsin(N_norm[0]/np.sqrt(1-np.square(N_norm[1])))
     return (phi_x, theta_y)
 
-def calculate_li(T: npt.NDArray, phi_x: float, theta_y: float, pi_plane: npt.NDArray, bi: npt.NDArray) -> npt.NDArray:
-    """Calculate the vector between the plate mount and the motor mount, for any motor. The configuration of the motor
-    is described with pi and bi. See `rotation_math.png` for more details.
+def calculate_li(T: npt.NDArray, phi_x: float, theta_y: float, pi_plat: npt.NDArray, bi: npt.NDArray) -> npt.NDArray:
+    """Calculate the vector between the plate mount and the motor mount, for any motor. The configuration of the 
+    mounting on the plate, and the mounting of the motor, is described with pi and bi respectively. 
+    See `rotation_math.png` for more details.
 
     Args:
         T (3 float vector): The vector with mag/dir between the center of the motor mounts, and the center of the plate
         phi_x (float): The angle of the tilt of the plate about the x-axis (rad)
         theta_y (float): The angle of the tilt of the plane about the y-axis (rad)
-        pi_plane (3 float vector): The vector describing the distance between plate mount and center of plate, in the plate's 
-        reference frame
+        pi_plat (3 float vector): The vector describing the distance between plate mount and center of plate, in the 
+        plate's reference frame
         bi (3 float vector): The vector describing the distance between the motor mount and center of plate, in the 
         body's reference frame
 
@@ -58,13 +61,13 @@ def calculate_li(T: npt.NDArray, phi_x: float, theta_y: float, pi_plane: npt.NDA
         3 float vector: The vector from motor mount -> plate mount
     """
     # Rotation matrix of A*B where A is x rotation (phi_x), B is y rotation (theta_y)
-    R_P_wrt_B = np.array(
+    R_p_wrt_b = np.array(
         [np.cos(theta_y), np.sin(theta_y)*np.sin(phi_x), np.sin(theta_y)*np.cos(phi_x)],
         [0, np.cos(phi_x), -np.sin(phi_x)],
         [-np.sin(theta_y), np.cos(theta_y)*np.sin(phi_x), np.cos(phi_x)*np.cos(theta_y)]
         )
     # Rotate the pi vector with the same rotation matrix as the unit K -> new normal vector
-    pi_body = R_P_wrt_B @ pi_plane
+    pi_body = R_p_wrt_b @ pi_plat
     li = T + pi_body - bi
     return li
 
