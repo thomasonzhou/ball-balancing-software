50ce789d3fd1b9cd4a59386387203a7d6743d1cf
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-09-21 11:21:31 -0400

    Link opencv libraries

--- include/pose_estimation.h
+++ include/pose_estimation.h
@@ -1,3 +1,3 @@
+#include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
-
-
+// #include <opencv2/objdetect/aruco_dictionary.hpp>
--- src/pose_estimation.cc
+++ src/pose_estimation.cc
@@ -1,6 +1,13 @@
 #include "pose_estimation.h"
 #include <iostream>
 
+void generateMarker(){
+   cv::Mat markerImage;
+   cv::aruco::Dictionary dictionary;
+}
+
 int main(){
     std::cout << "Starting pose estimation"<<std::endl;
+    generateMarker();
 }
+
