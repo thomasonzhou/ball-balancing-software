5c38a084f65c0e307216abb33763a34fb7fbef07
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-09-21 11:33:05 -0400

    Generate aruco markers

--- src/pose_estimation.cc
+++ src/pose_estimation.cc
@@ -2,8 +2,10 @@
 #include <iostream>
 
 void generateMarker(){
-   cv::Mat markerImage;
-   cv::aruco::Dictionary dictionary;
+    cv::Mat markerImage;
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    cv::aruco::generateImageMarker(dictionary, 23, 200, markerImage, 1);
+    cv::imwrite("marker23.png", markerImage);
 }
 
 int main(){
