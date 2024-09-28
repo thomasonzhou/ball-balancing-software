ee4d9bdc96cd5ad50ee27bcc23e6f66d607593d6
Olivia Ye <77518590+oliviawxye@users.noreply.github.com>
2024-09-28 19:56:44 -0400

    Fix python project dependencies (#4)
    #2 was pre-emptively merged without removing setup.py, this PR fixes that. Also provides some example code and an example module setup of how to import from a module, at all levels of directory in an editable format. For example:
    
    ```
    ├── python
    │   ├── kinematics
    │   ├── main.py
    │   └── utils
    │       └── helper.py
    ├── tests
    │   ├── test.py
    ```
    
    Covers examples of how to import:
    * From `main.py` into `test.py`
    * From `helper.py` to `main.py`
    
    ---------
    
    Co-authored-by: Thomason Zhou <thomason.zhou@uwaterloo.ca>

---
+++ python/kinematics/main.py
@@ -0,0 +1,11 @@
+"""Example main code. See code below for how this can possibly be used."""
+
+# TODO: DELETE SAMPLE CODE
+from kinematics.utils.helper import helper
+
+def some_math():
+    four = 2+2
+    print(f"Two plus two is: {four}")
+
+if __name__ == "__main__":
+    helper()
---
+++ python/tests/test.py
@@ -0,0 +1,8 @@
+"""Example test module. See code below for how this can possibly be used."""
+
+# TODO: DELETE SAMPLE CODE
+from kinematics.main import some_math
+
+if __name__ == "__main__":
+    print("If this goes right, I should see a number...")
+    some_math()
