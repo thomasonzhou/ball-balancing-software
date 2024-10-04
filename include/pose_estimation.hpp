d3917862f67979b4d411a4bec4d0cc063dba2172
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-10-04 11:41:29 -0400

    Add pose estimation basics

--- include/pose_estimation.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef POSE_ESTIMATION_H_
-#define POSE_ESTIMATION_H_
-
-#include <opencv2/opencv.hpp>
-const int ARUCOTAG_DICTIONARY = cv::aruco::DICT_6X6_250;
-const std::string CAMERA_PARAMS_FILE = "camera_params.yml";
-#endif
