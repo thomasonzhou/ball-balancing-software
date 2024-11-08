c92fd4f564d4b7d3ae6a81b23c7ad66472865542
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-11-08 16:02:15 -0500

    Update build instructions

--- include/camera_info.hpp
+++ include/camera_info.hpp
@@ -4,7 +4,7 @@
 #include <opencv2/objdetect/charuco_detector.hpp> 
 #include <opencv2/opencv.hpp>
 const int ARUCOTAG_DICTIONARY = cv::aruco::DICT_6X6_250;
-const std::string CAMERA_PARAMS_FILE = "logitech_calib.yml";
+const std::string CAMERA_PARAMS_FILE = "macbook_calib.yml";
 
 // Charuco board specs
 const int squaresX = 5;
