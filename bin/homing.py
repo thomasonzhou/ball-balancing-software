5b1d1113d78ec9e3c960c6164f35aec110c9b43d
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-11-10 17:52:16 -0500

    Add homing update

--- bin/homing.py
+++ bin/homing.py
@@ -18,15 +18,20 @@ def read_last_line(filename):
         last_line = f.readline().decode().strip()
     return last_line
 
+def read_aruco_data():
+    aruco_data = read_last_line(KINEMATICS_INFILE_NAME)
+    aruco_data_list = aruco_data.split(",")
+    if not aruco_data_list:
+        return None
+    return aruco_data_list # x, y, z, distance
 
 def level_platform():
     detect_aruco = Popen("./aruco_detection")
 
     calibrated = False
     while not calibrated:
-        aruco_data = read_last_line(KINEMATICS_INFILE_NAME)
-        x, y, z, distance = aruco_data.split(",")
-
+        x, y, z, distance = read_aruco_data()
+        print(x, y, z, distance)
 
 if __name__ == "__main__":
     print("Warning: this script requires C++ binaries.")
