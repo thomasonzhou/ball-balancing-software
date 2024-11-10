9f5b6a899aa3c69faaee732d4168f4115e7b63dd
Vincent Nguyen <vincentvnguyen@hotmail.com>
2024-11-10 16:56:18 -0500

    completed tele-op pipeline

--- python/tele-op/serial_forward.py
+++ python/tele-op/serial_forward.py
@@ -3,6 +3,7 @@ import time
 
 from parse_process import parse_coord, ascii_encode
 from pid.position_feedback import Controller
+from kinematics.kinematics import translate_dir_to_motor_angles
 
 pid = Controller()
 desired_coord = (0,0)
@@ -28,19 +29,23 @@ try:
                 actual_coord = parse_coord(decoded_data)
                 if actual_coord:
                     actual_x, actual_y = actual_coord
-                    print(f"RX: {decoded_data}")
+                    print(f"RX COORD: {decoded_data}")
                     
                     # Calulate control signal
-                    (dir_x, dir_y, theta_mag) = pid.calculate(desired_coord, actual_coord)
+                    dir_x, dir_y, theta_mag = pid.calculate(desired_coord, actual_coord)
                 # else:
                 #     dir_x, dir_y, theta_mag = 0,0,0
 
-                # Re-encode and send the ASCII-decoded data over the output port
-                    motor_output = ascii_encode((dir_x, dir_y, theta_mag))
+                    # Convert unit direction vector and control signal magnitude to motor angles
+                    motor_angles = translate_dir_to_motor_angles(dir_x, dir_y, theta_mag)
+
+                    # Re-encode and send the ASCII-decoded data over the output port
+                    motor_output = ascii_encode(motor_angles)
                     ser_out.write(motor_output)
 
                     # Debug: Confirm data was sent
-                    print(f"TX: {motor_output.decode('ascii')}")
+                    print(f"PID VECTOR: {(dir_x, dir_y, theta_mag)}")
+                    print(f"TX ANGLE: {motor_output.decode('ascii')}")
                 
             except UnicodeDecodeError:
                 print("non-ASCII data")
