0f173a4a74b3a0569d65bb62295bc73463cdc8e9
Olivia Ye <oliviawxye@gmail.com>
2024-11-10 09:45:45 -0500

    new teleop file

---
+++ python/kinematics/tele_op_kinematics.py
@@ -0,0 +1,10 @@
+"""Module for the kinematics pipeline for tele-op"""
+
+from kinematics.plate_kinematics import *
+
+def translate_dir_to_motor_angles(x_dir: float, y_dir: float, mag: float) -> tuple[float, float, float]:
+    
+
+
+
+
