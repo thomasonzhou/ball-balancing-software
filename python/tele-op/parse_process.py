2d3739d4c7dd9a1955725144d1e0b0b58f170e17
Vincent Nguyen <vincentvnguyen@hotmail.com>
2024-11-10 01:18:59 -0500

    added xy coordinate parsing and extraction from joystick

---
+++ python/tele-op/parse_process.py
@@ -0,0 +1,16 @@
+import re
+
+# Expected ASCII data in the format <x,y>
+format = re.compile(r"<(-?\d+\.\d+),\s*(-?\d+\.\d+)>")
+
+def parse_coord(data):
+    match = format.match(data)
+    if match:
+        x = float(match.group(1))
+        y = float(match.group(2))
+        return x, y
+    else:
+        print("Data format error")
+        return None
+
+
