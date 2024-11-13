cf4d82dc79d1cbac121ded04ebd9c9a9dcedc519
Olivia Ye <77518590+oliviawxye@users.noreply.github.com>
2024-11-12 21:29:20 -0500

    Tele-op Main Script Barebones (#11)
    teleop main script barebones

---
+++ python/hlc/homing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os
+from typing import List
+
+KINEMATICS_INFILE_NAME = "kinematics_input.txt"
+
+def read_last_line(filename):
+    num_newlines = 0
+    with open(filename, "rb") as f:
+        try:
+            f.seek(-2, os.SEEK_END)
+            while num_newlines < 1:
+                f.seek(-2, os.SEEK_CUR)
+                if f.read(1) == b"\n":
+                    num_newlines += 1
+        except OSError:
+            f.seek(0)
+        last_line = f.readline().decode().strip()
+    return last_line
+
+def read_aruco_data() -> List[str]:
+    aruco_data = read_last_line(KINEMATICS_INFILE_NAME)
+    aruco_data_list = aruco_data.split(",")
+    if not aruco_data_list:
+        return None
+    return aruco_data_list # x, y, z, distance
