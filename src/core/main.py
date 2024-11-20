3ee66e1b278b6e813bdd01c0719be0dcd7b3d792
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-11-20 02:52:49 -0500

    change to traj mode

--- src/core/main.py
+++ src/core/main.py
@@ -41,7 +41,9 @@ def main(operation_mode=OperationMode.COMPUTER_VISION, motors_on=True, debug_mod
     # --------------------------------------------------
     # Initialize Components
     # --------------------------------------------------
-    controller = pid.Controller(print_errors=debug_mode, dead_zone=True)
+    controller = pid.Controller(
+        pid_params=pid.PID_Mode.PathPlanning, print_errors=debug_mode, dead_zone=True
+    )
     planner = motion_planner.MotionPlanner()
     if operation_mode == OperationMode.COMPUTER_VISION:
         ball_detector = computer_vision.BallDetector(preview=debug_mode)
