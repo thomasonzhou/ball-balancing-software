a361b96618e5d112455bd9e5e6efa1e4816c6d22
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-11-19 22:12:21 -0500

    Tune again

--- src/pid/position_feedback.py
+++ src/pid/position_feedback.py
@@ -17,9 +17,9 @@ def saturate(control: float, sat_min: float, sat_max: float) -> float:
 
 class Controller:
     # Define PID gains and time interval
-    kp = 0.15
-    ki = 0
-    kd = 1.5
+    kp = 0.20
+    ki = 0.01
+    kd = 1.3
     dt = 0.1
     SAT_MAX_DEGREES = 9
     SAT_MIN_DEGREES = 0
