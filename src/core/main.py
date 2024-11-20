26a5c824b831715a158d76df9f4631803242c7ee
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-11-20 03:55:59 -0500

    triangle with golf ball

--- src/core/main.py
+++ src/core/main.py
@@ -137,15 +137,15 @@ def main(
 
 
 if __name__ == "__main__":
-    # main(
-    #     pid_mode=pid.PID_Mode.PathPlanning,
-    #     operation_mode=OperationMode.COMPUTER_VISION,
-    #     motion_planner=motion_planner.LoopType.TRIANGLE,
-    #     debug_mode=True,
-    # )
     main(
-        pid_mode=pid.PID_Mode.DisturbanceRejection,
+        pid_mode=pid.PID_Mode.PathPlanning,
         operation_mode=OperationMode.COMPUTER_VISION,
-        planner=motion_planner.LoopType.NONE,
+        motion_planner=motion_planner.LoopType.TRIANGLE,
         debug_mode=True,
     )
+    # main(
+    #     pid_mode=pid.PID_Mode.DisturbanceRejection,
+    #     operation_mode=OperationMode.COMPUTER_VISION,
+    #     planner=motion_planner.LoopType.NONE,
+    #     debug_mode=True,
+    # )
